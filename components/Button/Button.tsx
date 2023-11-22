97ce431a7403adb49f9780c8e7d016e18cfd7f0d
ananevandrei11 <ujlpjcbjlf2011@gmail.com>
2023-11-22 10:55:43 +0100

    17-animation-3

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -2,6 +2,7 @@ import clsx from 'clsx';
 import styles from './Button.module.css';
 import { ButtonProps } from './Button.props';
 import { ArrowRight } from '@/Icon';
+import { motion, useMotionValue } from 'framer-motion';
 
 export const Button = ({
   appearance,
@@ -9,12 +10,18 @@ export const Button = ({
   children,
   className,
   ...props
-}: ButtonProps) => (
-  <button
-    className={clsx(styles.button, styles[appearance], className)}
-    {...props}>
-    {arrow === 'left' && <ArrowRight />}
-    {children}
-    {arrow === 'right' && <ArrowRight />}
-  </button>
-);
+}: ButtonProps) => {
+  const scale = useMotionValue(1);
+
+  return (
+    <motion.button
+      whileHover={{ scale: 1.05 }}
+      style={{ scale }}
+      className={clsx(styles.button, styles[appearance], className)}
+      {...props}>
+      {arrow === 'left' && <ArrowRight />}
+      {children}
+      {arrow === 'right' && <ArrowRight />}
+    </motion.button>
+  );
+};
